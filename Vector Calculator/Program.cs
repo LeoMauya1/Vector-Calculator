94ec5fad9d2fe41efaabfb0d15f7e0bcb55291b5
Mauya <1717854@mukilteo.wednet.edu>
2024-01-26 13:21:50 -0800

    erugeoigj

--- Vector Calculator/Program.cs
+++ Vector Calculator/Program.cs
@@ -6,11 +6,24 @@ namespace Vector_Calculator
     {
         static void Main(string[] args)
         {
-            Vector test1 = new Vector(1, 2, 3);
-            Vector test2 = new Vector(-1, 2, -3);
+            Vector test1 = new Vector(-1, 2, 3);
+            Vector test2 = new Vector(1, -2, -3);
+           
             
             // This should be 0, 4, 0
             Console.WriteLine(Vector.Add(test1, test2));
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine(" subtraction : ");
+            Console.WriteLine(Vector.Subtract(test1, test2));
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine(Vector.Negate(test1));
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine(test1.GetMagnitude());
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine(test1.GetDirection());
+            System.Threading.Thread.Sleep(1000);
+
+           
         }
     }
 }
--- Vector Calculator/Vector.cs
+++ Vector Calculator/Vector.cs
@@ -23,34 +23,33 @@ namespace Vector_Calculator
             return $"<{x}, {y}, {z}>";
         }
 
-        public float GetMagnitude()
+        public double GetMagnitude()
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
         }
 
-        public float GetDirection()
+        public double GetDirection()
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+          
+          
+
+            return Math.Atan(y / x);
         }
 
         public static Vector Add(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
         }
 
         public static Vector Negate(Vector v)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+
+            return new Vector(v.x * -1, v.y * -1, v.z * -1);
         }
 
         public static Vector Subtract(Vector v1, Vector v2)
         {
-            // ADD CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            return new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
         }
 
         public static Vector Scale(Vector v)
